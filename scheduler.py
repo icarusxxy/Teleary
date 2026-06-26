06631572ad857d6163858d59bcb9d9b192b5bd9b
Yutung Chung <yutung.chung@d8ai.com>
2026-06-26 14:05:20 +0800

    Prevent duplicate reminders and add cooldown
    Skip reminders if an entry was already created today or if a reminder
    was sent within the last two hours.

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 from apscheduler.triggers.interval import IntervalTrigger
@@ -49,6 +50,19 @@ async def _random_reminder():
         return
 
     now = get_now()
+
+    today_entries = await db.get_entries_by_date(now.date())
+    if today_entries:
+        log.debug("reminder_skipped already_jotted today_entry_count={}", len(today_entries))
+        return
+
+    last_sent_str = await db.get_setting("last_reminder_sent")
+    if last_sent_str:
+        last_sent = datetime.fromisoformat(last_sent_str)
+        if (now - last_sent).total_seconds() < 7200:
+            log.debug("reminder_skipped cooldown active last_sent='{}'", last_sent_str)
+            return
+
     start = int(await db.get_setting("reminder_start") or "9")
     end = int(await db.get_setting("reminder_end") or "21")
 
@@ -56,6 +70,7 @@ async def _random_reminder():
         if random.random() < 0.30:
             msg = random.choice(REMINDER_POOL)
             await _bot.send_message(chat_id=_chat_id, text=msg)
+            await db.set_setting("last_reminder_sent", now.isoformat())
             log.info("reminder_sent hour={} message='{}'", now.hour, msg[:40])
         else:
             log.debug("reminder_skipped hour={} probability_miss", now.hour)
